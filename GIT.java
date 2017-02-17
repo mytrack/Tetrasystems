f3f5b6ca4c42a92a82e8598312cba6fe681d51ea
mytrack <tetrasys@icloud.com>
2017-02-17 16:15:25 +0900

    1st add GIT.java

---
+++ GIT.java
@@ -0,0 +1,43 @@
+<<GIT_OPERATION
+
+  
+== git ===========================================
+
+-------------------------------Workspace
+mkdir <Tetrasystems>  <----- Tetrasystems 작업이름
+cd <Tetrasystems>
+
+
+-------------------------------Create
+git init
+
+
+-------------------------------Connect
+git remote add origin https://github.com/tetrasys/<Tetrasystems>.git
+
+
+-------------------------------Work
+git add(rm) file
+git commit -m "msg"
+
+
+-------------------------------Confirm 
+git status 
+git log
+
+
+-------------------------------Upload 
+git push -u origin master  -> github.com repository = <Tetrasystems> 존재해야됨.
+
+
+-------------------------------Download
+git clone https://github.com/tetrasys/<?>
+
+
+-------------------------------Sync
+git pull 
+
+== git ===========================================
+
+
+GIT_OPERATION
