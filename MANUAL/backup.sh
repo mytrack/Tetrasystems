b26326352cca4812b15eed4203dbd9b441f6b434
Space and Time Code <tetrasys@icloud.com>
2017-02-18 03:18:02 +0900

    backup modified

--- MANUAL/backup.sh
+++ MANUAL/backup.sh
@@ -2,4 +2,4 @@
 /bin/cp ~/MacBook_crontab.java ~/Tetrasystems/MANUAL/
 
 /bin/cp ~/Traspix.sh ~/Tetrasystems/MANUAL
-/bin/cp ~/backup.sh ~/Tetrasystems/MANUAL/
+/bin/cp ~/backup.sh ~/Tetrasystems/MANUAL
