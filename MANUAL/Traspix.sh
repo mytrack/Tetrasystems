b1367edd9854edad1b5cad3ea96027979013f94f
Space and Time Code <tetrasys@icloud.com>
2017-02-17 23:57:09 +0900

    backup.sh modified

--- MANUAL/Traspix.sh
+++ MANUAL/Traspix.sh
@@ -1107,41 +1107,6 @@ UNIX_TECHS
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 <<TRASPIX_INSTRUCTION_MANUAL
 
 
@@ -1264,48 +1229,3 @@ C- jump CS-
 SUBLIME_KEY
 
 
-
-
-<<GIT_OPERATION
-
-  
-== git ===========================================
-
--------------------------------Workspace
-mkdir <Tetrasystems>  <----- Tetrasystems 작업이름
-cd <Tetrasystems>
-
-
--------------------------------Create
-git init
-
-
--------------------------------Connect
-git remote add origin https://github.com/tetrasys/<Tetrasystems>.git
-
-
--------------------------------Work
-git add(rm) file
-git commit -m "msg"
-
-
--------------------------------Confirm 
-git status 
-git log
-
-
--------------------------------Upload 
-git push -u origin master  -> github.com repository = <Tetrasystems> 존재해야됨.
-
-
--------------------------------Download
-git clone https://github.com/tetrasys/<?>
-
-
--------------------------------Sync
-git pull 
-
-== git ===========================================
-
-
-GIT_OPERATION
---
+++ MANUAL/backup.sh
@@ -0,0 +1,5 @@
+
+/bin/cp ~/MacBook_crontab.java ~/Tetrasystems/MANUAL/
+
+/bin/cp ~/Traspix.sh ~/Tetrasystems/MANUAL
+/bin/cp ~/backup.sh ~/Tetrasystems/MANUAL/
