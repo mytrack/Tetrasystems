b1204e1dd4ec4751dcfb4ce9d605dc7e6e54d0af
Space and Time Code <tetrasys@icloud.com>
2017-02-17 16:18:46 +0900

    add VIP.java

---
+++ VIP.java
@@ -0,0 +1,54 @@
+<<VERY_IMPORTANT
+
+
+실행하기전 pi 마운트
+
+emergency.sh
+
+rsync ~/Documents/EMERGENCY_Raspberry_Pi /Volumes/pi --progress -av 
+
+
+
+12345 포트 열기
+
+
+fileupload install)
+
+ImportError: No module named sqlalchemy.orm
+ImportError: No module named pysrt
+ImportError: No module named pymysql
+
+sudo pip install sqlalchemy
+sudo pip install pysrt
+sudo pip install pymysql
+
+---------------------> 여기까지 파일업로드는 가능
+
+sudo apt-get install -y  mysql-server
+
+DB생성
+
+app.py 루트 패스워드 조심   
+
+
+Using delimiter:	;
+Server version:		5.5.54-0+deb8u1 (Raspbian)
+Protocol version:	10
+Connection:		Localhost via UNIX socket
+Server characterset:	latin1
+Db     characterset:	latin1
+Client characterset:	utf8
+Conn.  characterset:	utf8
+UNIX socket:		/var/run/mysqld/mysqld.sock
+
+
+영문 데이터는 문제없지만  한글은 ?????로 보이는 문제
+
+mysql_conf /etc/mysql/my.cnf에서 처럼 3개를 추가하면 된다
+
+status 확인했을때  Db characterset: latin1
+라면 데이터베이스를 삭제후 다시 생성한다.
+
+
+
+VERY_IMPORTANT
