42caf8372da6279ad1d3d873597a40269f0f7e8e
Space and Time Code <tetrasys@icloud.com>
2017-02-18 14:30:41 +0900

    here is 충주휴게소

--- MANUAL/MacBook_crontab.java
+++ MANUAL/MacBook_crontab.java
@@ -1,11 +1,13 @@
-
 DATEVAR=date +20%y%m%d_%H%M%S
 #DATEVAR=date +%Y-%m-%d_%H:%M:%S
+
+
 */5 * * * * echo $($DATEVAR) >> ~/Tetrasystems/my_cron_7.log 
 
 
 0,7,17,27,37,47,57 * * * * echo $($DATEVAR) >> ~/Tetrasystems/cron_timestamp_mytrack.log 
 */7 * * * * ~/backup.sh >/dev/null 2>&1
+
 
 
 #*/30 9,10,11,12,13,14,15,16,17 * * * ~/backup.sh >/dev/null 2>&
--- MANUAL/backup.sh
+++ MANUAL/backup.sh
@@ -1,4 +1,3 @@
-
 /bin/cp ~/MacBook_crontab.java ~/Tetrasystems/MANUAL/
 
 /bin/cp ~/Traspix.sh ~/Tetrasystems/MANUAL
