cbc9960699ab88e37d0113d99d80eaed68c03762
Space and Time Code <tetrasys@icloud.com>
2017-02-19 16:48:43 +0900

    alias_assign_profile

---
+++ MANUAL/ALIAS.java
@@ -0,0 +1,18 @@
+<<ALIAS_ASSIGN_PROFILE
+
+
+
+alias rm='rm -i'
+alias del='rm -i'
+alias sync_emergencyrpi='~/sync_emergencyrpi.sh'
+alias vipro='vi ~/Tetrasystems/MANUAL/ALIAS.java ~/.bash_profile'
+alias .pro='source ~/.bash_profile'
+
+alias 'ls=ls -GFh'
+export PS1="\[\033[36m\]\u\[\033[m\]@\[\033[32m\]\h:\[\033[33;1m\]\w\[\033[m\]\$ "
+export CLICOLOR=1
+export LSCOLORS=ExFxBxDxCxegedabagacad
+                                           
+
+
+ALIAS_ASSIGN_PROFILE
