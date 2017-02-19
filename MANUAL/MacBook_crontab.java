f2ef32774317d03ff3744ee4ec564fa3941c5ed1
Space and Time Code <tetrasys@icloud.com>
2017-02-19 12:31:29 +0900

    crontab configuration modified

--- MANUAL/MacBook_crontab.java
+++ MANUAL/MacBook_crontab.java
@@ -2,14 +2,17 @@ DATEVAR=date +20%y%m%d_%H%M%S
 #DATEVAR=date +%Y-%m-%d_%H:%M:%S
 
 
-*/5 * * * * echo $($DATEVAR) >> ~/Tetrasystems/my_cron_7.log 
+# specify min
+#5,15,15,35,45,55 * * * * echo $($DATEVAR) >> ~/Tetrasystems/mytrack.log 
+*/5 * * * * echo $($DATEVAR) >> ~/Tetrasystems/mytrack_per_5_min.log 
 
 
-0,7,17,27,37,47,57 * * * * echo $($DATEVAR) >> ~/Tetrasystems/cron_timestamp_mytrack.log 
-*/7 * * * * ~/backup.sh >/dev/null 2>&1
+#0,7,17,27,37,47,57 * * * * echo $($DATEVAR) >> ~/Tetrasystems/cron_timestamp_mytrack.log 
+*/10 * * * * echo $($DATEVAR) >> ~/Tetrasystems/cron_backup_timestamp_mytrack.log 
+*/10 * * * * ~/backup.sh >/dev/null 2>&1
 
 
-
+# specify hour 
 #*/30 9,10,11,12,13,14,15,16,17 * * * ~/backup.sh >/dev/null 2>&
 #*/5 18,19,20,21,22,23,0,1,2 * * * ~/backup.sh >/dev/null 2>&1
 
