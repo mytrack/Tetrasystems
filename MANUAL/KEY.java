3d4c5c93217996c536f5c63be6a5274c9ed992d8
Space and Time Code <tetrasys@icloud.com>
2017-03-08 15:31:06 +0900

    UNIX KEY

---
+++ MANUAL/KEY.java
@@ -0,0 +1,25 @@
+<<SUBLIME_KEY
+
+
+
+sublime text) settings -- user)
+Vintage
+"open_files_in_new_window": false,
+
+
+#p #r #Sr ^g #g #Sg
+C- jump CS- 
+#[ indent ]
+#/ comment 
+#A[folding]
+#Ar replace
+
+
+
+#CAdown go definition
+
+
+
+
+
+SUBLIME_KEY
