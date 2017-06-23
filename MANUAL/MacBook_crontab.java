9904247c9fcbcee6ca5e52d842fa71709ddc2b75
Space and Time Code <tetrasys@icloud.com>
2017-06-23 23:01:43 +0900

    june mytrack

--- MANUAL/MacBook_crontab.java
+++ MANUAL/MacBook_crontab.java
@@ -1,18 +1,19 @@
+# declaration variable
 DATEVAR=date +20%y%m%d_%H%M%S
 #DATEVAR=date +%Y-%m-%d_%H:%M:%S
 
 
 # specify min
-#5,15,15,35,45,55 * * * * echo $($DATEVAR) >> ~/Tetrasystems/mytrack.log 
-*/5 * * * * echo $($DATEVAR) >> ~/Tetrasystems/mytrack_per_5_min.log 
+#5,15,15,35,45,55 * * * * echo $($DATEVAR) >> ~/Tetrasystems/mytrack.log
+*/10 * * * * echo $($DATEVAR) >> ~/Tetrasystems/mytrack_per_10_min.log
 
 
-#0,7,17,27,37,47,57 * * * * echo $($DATEVAR) >> ~/Tetrasystems/cron_timestamp_mytrack.log 
-*/10 * * * * echo $($DATEVAR) >> ~/Tetrasystems/cron_backup_timestamp_mytrack.log 
-*/10 * * * * ~/backup.sh >/dev/null 2>&1
+# backup timestamp
+#0,7,17,27,37,47,57 * * * * echo $($DATEVAR) >> ~/Tetrasystems/cron_timestamp_mytrack.log
+*/15 * * * * echo $($DATEVAR) >> ~/Tetrasystems/cron_backup_timestamp_mytrack.log
+*/15 * * * * ~/backup.sh >/dev/null 2>&1
 
 
-# specify hour 
+# specify hour
 #*/30 9,10,11,12,13,14,15,16,17 * * * ~/backup.sh >/dev/null 2>&
 #*/5 18,19,20,21,22,23,0,1,2 * * * ~/backup.sh >/dev/null 2>&1
-
--- MANUAL/backup.sh
+++ MANUAL/backup.sh
@@ -1,3 +1,5 @@
+#
 /bin/cp ~/MacBook_crontab.java ~/Tetrasystems/MANUAL/
 
+#
 /bin/cp ~/*.sh ~/Tetrasystems/MANUAL
